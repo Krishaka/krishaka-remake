14bbc8eaf9ead3910de39a5d16afcf6e0d14e769
Sai Rohit <sairohit11.s@gmail.com>
2024-02-07 19:55:42 +0530

    Add number carousal and update styles

--- modules/home-page/home-carousal/index.tsx
+++ modules/home-page/home-carousal/index.tsx
@@ -6,43 +6,93 @@ import {
   carousalTrackCss,
   carousalWrapperCss,
   homeCarousalCss,
-  itemCounterCss
+  itemCounterCss,
+  numCarousalWrapperCss,
+  numItemCss
 } from "@/modules/home-page/home-carousal/styles";
 import { commonHeaderCss } from "@/styles/common-styles";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 
 export default function HomeCarousal() {
   const carousalItemNum = useRef<number>(productList.length);
   const carousalRef = useRef<HTMLDivElement>(null);
   const trackRef = useRef<HTMLDivElement>(null);
-  const [itemNum, setItemNum] = useState<number>(1);
+  const fontPixelWidth = useRef<number>(12.22);
 
   const scrollLength = useRef<number>(0);
   useEffect(() => {
     const track = trackRef.current;
+    const carousal = carousalRef.current;
     scrollLength.current = (track?.scrollWidth ?? 0) / carousalItemNum.current;
+
     const resizeHandler = () => {
       scrollLength.current = (track?.scrollWidth ?? 0) / carousalItemNum.current;
     };
+
+    const obvOptions: IntersectionObserverInit = {
+      root: document
+    };
+
+    const leftArrow = carousal?.previousSibling;
+    const rightArrow = carousal?.nextSibling;
+
+    const obvCallback: IntersectionObserverCallback = (entries) => {
+      if (entries[0].isIntersecting) {
+        const classAdder = (itemClassName: string, arrow: ChildNode | HTMLDivElement | null | undefined) => {
+          if (entries[0].target.classList.contains(itemClassName)) {
+            (arrow as HTMLDivElement).classList.add("arrow-inactive");
+          } else {
+            (arrow as HTMLDivElement).classList.remove("arrow-inactive");
+          }
+        };
+        classAdder("product-item-0", leftArrow);
+        classAdder(`product-item-${carousalItemNum.current - 1}`, rightArrow);
+      }
+    };
+
+    const obv: IntersectionObserver = new IntersectionObserver(obvCallback, obvOptions);
+
+    if (track) {
+      const carousalItems = Array.from(track.querySelectorAll(".product-item"));
+      for (const carousalItem of carousalItems) {
+        obv.observe(carousalItem);
+      }
+    }
     addEventListener("resize", resizeHandler);
 
-    return () => removeEventListener("resize", resizeHandler);
-  }, [itemNum]);
+    return () => {
+      obv.disconnect();
+      removeEventListener("resize", resizeHandler);
+    };
+  }, []);
 
   const clickHandler = (num: number) => {
     return function returnHandler() {
       const carousal = carousalRef.current;
+      const numberCarousal = document.querySelector(".num-carousal");
       const scrollNum = scrollLength.current ?? 0;
       if (carousal) {
-        setItemNum(itemNum + num * 1);
         carousal.scrollTo({
           left: carousal.scrollLeft + num * scrollNum,
           behavior: "smooth"
         });
       }
+      if (numberCarousal) {
+        numberCarousal.scrollTo({
+          left: numberCarousal.scrollLeft + num * fontPixelWidth.current,
+          behavior: "smooth"
+        });
+      }
     };
   };
+
+  const numberMapper = (_: unknown, index: number) => (
+    <span key={index} css={numItemCss}>
+      {index + 1}
+    </span>
+  );
+
   const carousalMapper = (product: (typeof productList)[0], index: number) => {
     const { productDesc, productImg, productName } = product;
     return (
@@ -52,7 +102,7 @@ export default function HomeCarousal() {
         cardHeading={productName}
         imgSrc={productImg}
         key={`product-item-` + index}
-        name={"product-item-" + index}
+        name={"product-item product-item-" + index}
       />
     );
   };
@@ -60,7 +110,11 @@ export default function HomeCarousal() {
     <div css={homeCarousalCss}>
       <h2 css={commonHeaderCss("var(--color-text-default)")}>Our Products</h2>
       <div css={carousalWrapperCss}>
-        <FaCircleChevronLeft css={carousalArrowCss} className="arrow left-arrow" onClick={clickHandler(-1)} />
+        <FaCircleChevronLeft
+          css={carousalArrowCss}
+          className="arrow left-arrow arrow-inactive"
+          onClick={clickHandler(-1)}
+        />
         <div css={carousalContainerCss} ref={carousalRef}>
           <div css={carousalTrackCss(carousalItemNum.current)} ref={trackRef}>
             {productList.map(carousalMapper)}
@@ -69,8 +123,11 @@ export default function HomeCarousal() {
         <FaCircleChevronRight css={carousalArrowCss} className="arrow right-arrow" onClick={clickHandler(1)} />
       </div>
       <div css={itemCounterCss}>
-        <span>{itemNum}</span>
-        <span> / {carousalItemNum.current}</span>
+        <div css={numCarousalWrapperCss(`${fontPixelWidth.current}px`)} className="num-carousal">
+          {Array(carousalItemNum.current).fill(null).map(numberMapper)}
+        </div>
+        <span>/</span>
+        <span>{carousalItemNum.current}</span>
       </div>
     </div>
   );
--- modules/home-page/home-carousal/styles.ts
+++ modules/home-page/home-carousal/styles.ts
@@ -52,25 +52,46 @@ export const carousalTrackCss = (colNum: number) => css`
   }
 `;
 
-export const itemCounterCss = css`
-  display: none;
-  ${mediaQuery.miniDesktop} {
-    margin: auto;
-    display: block;
-    width: 100%;
-    text-align: center;
-    color: var(--color-bg-secondary);
-    font-size: var(--font-size-small);
-  }
-`;
-
 export const carousalArrowCss = css`
   font-size: var(--padding-normal);
   cursor: pointer;
   ${mediaQuery.mobile} {
     font-size: var(--font-size-icon);
   }
-  &.inactive {
-    background-color: red;
+  &.arrow-inactive {
+    cursor: not-allowed;
+    opacity: 0.25;
   }
+`;
+
+export const itemCounterCss = css`
+  display: none;
+  ${mediaQuery.miniDesktop} {
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    width: 100%;
+    text-align: center;
+    color: var(--color-bg-secondary);
+    font-size: var(--font-size-small);
+    margin: 0 auto;
+  }
+`;
+
+export const numCarousalWrapperCss = (num: string) => css`
+  --font-width: ${num};
+  width: var(--font-width);
+  padding: 10px 0;
+  display: flex;
+  overflow-x: scroll;
+  ::-webkit-scrollbar {
+    height: 0;
+  }
+`;
+
+export const numItemCss = css`
+  width: var(--font-width);
+  flex-shrink: 0;
 `;
