496fba170ec1c2199e4fb31d440c5b9b466b0381
storm <satstormlink@gmail.com>
2024-10-17 19:16:55 +0530

    about page update

--- modules/about-page/about-banner/index.tsx
+++ modules/about-page/about-banner/index.tsx
@@ -2,5 +2,11 @@ import ImageWrapper from "@/components/image-wrapper";
 import { aboutBannerCss } from "@/modules/about-page/about-banner/styles";
 
 export default function AboutBanner() {
-  return <ImageWrapper src="/images/about.jpg" alt="about-image" extraStyles={aboutBannerCss}></ImageWrapper>;
+  return (
+    <ImageWrapper
+      src="/images/home-model-bg.jpg"
+      alt="about-image"
+      extraStyles={aboutBannerCss}
+    ></ImageWrapper>
+  );
 }
